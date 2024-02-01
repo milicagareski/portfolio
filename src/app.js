886eff38f34bd4277211389e7522357939c0323f
Milica Gareski <milicakiprovska77@gmail.com>
2024-02-01 17:11:55 +0100

    getting git api

--- src/app.js
+++ src/app.js
@@ -84,6 +84,7 @@ function router(page) {
         .then((response) => response.text())
         .then((my_work) => {
           container.innerHTML = my_work;
+          getApiFromGithub();
         })
         .catch((error) => {
           handleError();
@@ -101,32 +102,70 @@ function router(page) {
         });
       break;
     case "admin":
-      fetch(`../templates/${page}.html`)
-        .then((response) => response.text())
-        .then((admin) => {
-          container.innerHTML = admin;
-          openDashboard();
-        })
-        .catch((error) => {
-          handleError();
-        });
+      const session = JSON.parse(localStorage.getItem("appSession"));
+      if (session && session.loggedIn) {
+        fetch(`../templates/dashboard.html`)
+          .then((response) => response.text())
+          .then((admin) => {
+            container.innerHTML = admin;
+            // openDashboard();
+          })
+          .catch((error) => {
+            handleError();
+          });
+      } else {
+        fetch(`../templates/admin.html`)
+          .then((response) => response.text())
+          .then((admin) => {
+            container.innerHTML = admin;
+            // openDashboard();
+          })
+          .catch((error) => {
+            handleError();
+          });
+      }
       break;
-    // case "dashboard":
-    //   fetch(`../templates/${page}.html`)
-    //     .then((response) => response.text())
-    //     .then((dashboard) => {
-    //       container.innerHTML = dashboard;
-    //       fromDashboardToHomePage();
-    //       clearDashboard();
-    //     })
-    //     .catch((error) => {
-    //       handleError();
-    //     });
-    //   break;
 
     default:
       console.log("Page does not exist");
   }
+}
+
+function login() {
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+  fetch("http://localhost:3000/login", {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "POST",
+    body: [
+      `username=${encodeURIComponent(username.value)}`,
+      `password=${encodeURIComponent(password.value)}`,
+    ].join("&"),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      const session = {
+        loggedIn: data,
+      };
+
+      localStorage.setItem("appSession", JSON.stringify(session));
+      if (data === "true") {
+        router("admin");
+      } else {
+        const wrongLogin = document.getElementById("wrong_login");
+        wrongLogin.textContent = "Invalid login. Try again.";
+      }
+    })
+    .catch((error) => {
+      handleError();
+    });
+}
+
+function logout() {
+  localStorage.removeItem("appSession");
+  router("admin");
 }
 
 function handleError(msg = "404 Page Not Found") {
@@ -158,49 +197,37 @@ function setMySkills() {
   });
 }
 
-function openDashboard() {
-  const btn = document.getElementById("login_btn");
-  btn.addEventListener("click", changePage);
+// function openDashboard() {
+//   const btn = document.getElementById("login_btn");
+//   btn.addEventListener("click", changePage);
 
-  function changePage() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const wrong_login = document.getElementById("wrong_login");
-    const admin = document.getElementById("admin");
+//   function changePage() {
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
+//     const wrong_login = document.getElementById("wrong_login");
+//     const admin = document.getElementById("admin");
 
-    if (username === "milicagareski" && password === "123456789") {
-      fetch(`../templates/dashboard.html`)
-        .then((response) => response.text())
-        .then((dashboard) => {
-          admin.innerHTML = dashboard;
-          fromDashboardToHomePage();
-          getMessege();
-        })
-        .catch((error) => {
-          handleError();
-        });
-    } else {
-      wrong_login.innerHTML =
-        "Please write your username and password correctly";
-    }
-  }
-  function fromDashboardToHomePage() {
-    const btn = document.getElementById("dashboard_btn");
-    btn.addEventListener("click", init);
-  }
-  function clearDashboard() {
-    const clear_btn = document.getElementById("clear_btn");
-    console.log(clear_btn);
-    clear_btn.addEventListener("click", deleteItems);
-    function deleteItems() {
-      const messeges = document.querySelectorAll("new_messege");
-      messeges.forEach((mes) => {
-        mes.innerHTML = "";
-      });
-    }
-  }
-  // clearDashboard();
-}
+//     if (username === "milicagareski" && password === "123456789") {
+//       fetch(`../templates/dashboard.html`)
+//         .then((response) => response.text())
+//         .then((dashboard) => {
+//           admin.innerHTML = dashboard;
+//           fromDashboardToHomePage();
+//           getMessege();
+//         })
+//         .catch((error) => {
+//           handleError();
+//         });
+//     } else {
+//       wrong_login.innerHTML =
+//         "Please write your username and password correctly";
+//     }
+//   }
+//   function fromDashboardToHomePage() {
+//     const btn = document.getElementById("dashboard_btn");
+//     btn.addEventListener("click", init);
+//   }
+// }
 
 function displayMessege() {
   const btn = document.getElementById("sent_btn");
@@ -211,18 +238,67 @@ function displayMessege() {
     const senderEmail = document.getElementById("email").value;
     const messege = document.getElementById("messege").value;
 
-    localStorage.setItem("senderName", senderName);
-    window.location.href = "dashboard.html";
+    const sendMessege = [senderName, senderEmail, messege];
+
+    localStorage.setItem("sendMessege", sendMessege);
+    const success = document.getElementById("success");
+    success.textContent = "Thank you for contacting me";
+    setTimeout(() => {
+      success.textContent = "";
+    }, 5000);
   }
 }
 
 function getMessege() {
   const wrapper = document.querySelector("#messeges");
-  const messege = document.createElement("h1");
-  messege.setAttribute("class", "new_messege");
-  let newMessege = localStorage.getItem("senderName");
-  messege.innerHTML = newMessege;
-  wrapper.appendChild(messege);
+  let newMessege = localStorage.getItem("sendMessege").split(",");
+  newMessege.map((item) => {
+    let element = document.createElement("h1");
+    element.setAttribute("class", "new_messege");
+    element.textContent = item;
+    wrapper.appendChild(element);
+  });
+}
+
+async function getApiFromGithub() {
+  const apiUrl = "https://api.github.com/users/milicagareski/repos";
+  let urlProjects = [];
+  let myProjects = document.querySelector(".work_wrapper");
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const projects = await response.json();
+    renderProjects(projects);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  function renderProjects(projects) {
+    let allProjects = [];
+    for (let project of projects) {
+      for (let key in project) {
+        if (key === "html_url") {
+          allProjects.push(project[key]);
+        }
+      }
+    }
+    allProjects.map((project, index) => {
+      const element = document.createElement("div");
+      const img = document.createElement("img");
+      img.src = `../img/img-${index}.png`;
+      img.alt = `project-photo`;
+      const link = document.createElement("a");
+      link.href = project;
+      link.setAttribute("target", "_blank");
+      link.innerHTML = "view github repo";
+      element.appendChild(img);
+      element.appendChild(link);
+      myProjects.appendChild(element);
+    });
+  }
 }
 
 function init() {
